36745e874a8dbb2d501e8db10e4a2b7202f2ffa7
vicky <vicky@localhost>
2024-11-10 13:46:18 +0530

    commit

---
+++ SB-03-REST-API-1/src/main/java/com/vicky/controller/DepartmentController.java
@@ -0,0 +1,11 @@
+package com.vicky.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+
+
+}
